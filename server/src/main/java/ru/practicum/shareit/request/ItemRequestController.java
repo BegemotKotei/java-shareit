aa8f81d4104dc885a99d53c9851cafa1e9a4e42d
Ilya Drugov <thehardkor@yandex.ru>
2023-05-01 18:21:11 +0700

    16sprint.1 version: feat: new logic.

---
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -0,0 +1,57 @@
+package ru.practicum.shareit.request;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.request.dto.AnswerItemRequestDto;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.service.ItemRequestService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/requests")
+@RequiredArgsConstructor
+public class ItemRequestController {
+
+    private final ItemRequestService itemRequestService;
+
+    @PostMapping
+    public ResponseEntity<AnswerItemRequestDto> createItemRequest(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                                                  @RequestBody ItemRequestDto itemRequestDto) {
+        log.info("POST /requests : user ID {} creates itemRequest from DTO - {}", userId, itemRequestDto);
+        return ResponseEntity.ok().body(itemRequestService.createItemRequest(userId, itemRequestDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AnswerItemRequestDto>> getUsersItemRequests(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("GET /requests : get list of itemRequests by user ID {}", userId);
+        return ResponseEntity.ok().body(itemRequestService.getUsersItemRequests(userId));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<AnswerItemRequestDto>> getItemRequests(
+            @RequestHeader("X-Sharer-User-Id") Long userId,
+            @RequestParam(value = "from", defaultValue = "0", required = false) int from,
+            @RequestParam(value = "size", defaultValue = "10", required = false) int size) {
+        log.info("GET /requests/all?from={}&size={} : get list of itemRequests, user ID {}", from, size, userId);
+        return ResponseEntity.ok().body(itemRequestService.getItemRequests(userId, PageRequest.of(from / size, size)));
+    }
+
+    @GetMapping("/{requestId}")
+    public ResponseEntity<AnswerItemRequestDto> getItemRequest(@PathVariable("requestId") Long requestId,
+                                               @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("GET /requests/{} : get itemRequest by ID, user ID {}", requestId, userId);
+        return ResponseEntity.ok().body(itemRequestService.getItemRequestById(requestId, userId));
+    }
+}
